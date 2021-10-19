0585d46be5d50e6e464de82f6da8d70164f90768
Ksenx73 <hello@itcampe.com>
2021-10-19 11:40:25 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,9 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        //напишите тут ваш код
+    }
+}
